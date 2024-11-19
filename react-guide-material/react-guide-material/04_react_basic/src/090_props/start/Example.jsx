9ba351cab9bab35b13a94a4854ba52671541bb71
尚人 <99806127+NaotoYoshida1@users.noreply.github.com>
2024-11-19 14:17:40 +0900

    20241119_1417
    20241119_1417

--- react-guide-material/react-guide-material/04_react_basic/src/090_props/start/Example.jsx
+++ react-guide-material/react-guide-material/04_react_basic/src/090_props/start/Example.jsx
@@ -1,5 +1,13 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+// const Example = () => <Child color="red" />;
+const Example = () => {
+    return (
+        <>
+            <Child color="blue" />
+            <Child color="red" />
+        </>
+    )
+};
 
 export default Example;
